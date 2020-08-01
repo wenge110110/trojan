93cdc24e7d3c95063a3fa9ec578b4355a004b98f
wenge110110 <66159009+wenge110110@users.noreply.github.com>
2020-08-01 18:34:53 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -74,14 +74,14 @@ if test -s /etc/nginx/nginx.conf; then
 		2)
 		餐饮店铺外卖网站
 		;;
-旅游网站\(\){
+旅游网站"()"{
           rm -rf /usr/share/nginx/html/*
 	  cd /usr/share/nginx/html/
 	  wget https://github.com/wenge110110/trojan/raw/master/web.zip
 	  unzip web.zip
 }
 		;;
-餐饮店铺外卖网站\(\){
+餐饮店铺外卖网站"()"{
           rm -rf /usr/share/nginx/html/*
 	  cd /usr/share/nginx/html/
 	  wget https://github.com/wenge110110/trojan/raw/master/foodbuzz.zip
